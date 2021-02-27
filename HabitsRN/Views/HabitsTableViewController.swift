ac794ceb9c811d32bdf59b5530cf781799309f68
Matthew Hall <matthewhall2943@gmail.com>
2021-02-27 11:17:18 -0700

    Learned how to populate the tableView with an array of data

--- HabitsRN/Views/HabitsTableViewController.swift
+++ HabitsRN/Views/HabitsTableViewController.swift
@@ -14,16 +14,28 @@ class HabitsTableViewController: UITableViewController {
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    // Return the number of rows for the given section
+    override func tableView(_ tabelView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return names.count
+        
     }
-    */
+    
+    // Return the UITableViewCell for the given indexPath
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        var cell: UITableViewCell
+        if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell"){
+            cell = dequeueCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        let name = names[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
 
+    
+    //test array
+    var names: [String] = ["Alan", "Braus", "Max", "Libby", "Matthew"]
 }
