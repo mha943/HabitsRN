a59912ec8d1a6ff611cb1db0ce4ca27033f458c8
Matthew Hall <matthewhall2943@gmail.com>
2021-02-26 17:15:51 -0700

    Update SceneDelegate.swift
    Set up the main screen with the bounds of the screen. set the created viewController to the main controller

--- HabitsRN/SceneDelegate.swift
+++ HabitsRN/SceneDelegate.swift
@@ -16,7 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Create a set the window to be the same size as teh screen
+        guard let windowScene = (scene as? UIWindowScene) else{return}
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // Create an instance of the main view controller
+        let mainController = UIViewController()
+        mainController.view.backgroundColor = .blue
+        
+        // Tell the window to load the main controller as it's root view
+        window?.rootViewController = mainController
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
