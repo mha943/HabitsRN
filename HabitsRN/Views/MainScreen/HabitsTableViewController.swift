640640184e5beb482c9e6fe334e38c8638434375
Matthew Hall <matthewhall2943@gmail.com>
2021-03-06 18:02:32 -0700

    set up persistence layer functions to delete, swap, and reload. altered tableHabitsViewController to accept the persistent layer

--- HabitsRN/Views/MainScreen/HabitsTableViewController.swift
+++ HabitsRN/Views/MainScreen/HabitsTableViewController.swift
@@ -9,29 +9,37 @@ import UIKit
 
 class HabitsTableViewController: UITableViewController {
     //test array
-    var names: [String] = ["Alan", "Braus", "Max", "Libby", "Matthew"]
-    //test habits
-    var habits: [Habit] = [
-        Habit(title: "go to bed before 10pm", image: Habit.Images.book),
-        Habit(title: "exercise period, cuz i don't want to get fat",image: Habit.Images.book),
-        Habit(title: "work on capstone project", image: Habit.Images.book),
-    ]
+//    var names: [String] = ["Alan", "Braus", "Max", "Libby", "Matthew"]
+//    //test habits
+//    var habits: [Habit] = [
+//        Habit(title: "go to bed before 10pm", image: Habit.Images.book),
+//        Habit(title: "exercise period, cuz i don't want to get fat",image: Habit.Images.book),
+//        Habit(title: "work on capstone project", image: Habit.Images.book),
+//    ]
+    
+    private var persistence = PersistenceLayer()
     
     // Return the number of rows for the given section
     override func tableView(_ tabelView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return habits.count
+        return persistence.habits.count
         
     }
     
     // Return the UITableViewCell for the given indexPath
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell( withIdentifier: HabitTableViewCell.identifier,for: indexPath) as! HabitTableViewCell
-        let habit = habits[indexPath.row]
+        let habit = persistence.habits[indexPath.row]
         cell.configure(habit)
         return cell
     }
 
-    
+    // Calls the reload function from the persistence layer and reloads the table with it
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        persistence.setNeedsToReloadHabits()
+        tableView.reloadData()
+        
+    }
     
     
     override func viewDidLoad() {
--- HabitsRN/Views/PersistenceLayer.swift
+++ HabitsRN/Views/PersistenceLayer.swift
@@ -56,4 +56,52 @@ struct PersistenceLayer{
         userDefaults.set(habitsData, forKey: PersistenceLayer.userDefaultHabitsKeyValue)
         userDefaults.synchronize()
     }
+    
+    mutating func delete(_ habitIndex: Int){
+        self.habits.remove(at: habitIndex)
+        self.saveHabits()
+    }
+    
+    mutating func markHabitAsCompleted(_ habitIndex: Int) -> Habit{
+        var updatedHabit = self.habits[habitIndex]
+        guard updatedHabit.completedToday == false else { return updatedHabit}
+        
+        updatedHabit.numberOfCompletions += 1
+        
+        // set up streak variable for the habit
+        if let lastCompletionDate = updatedHabit.lastCompletionDate, lastCompletionDate.isYesterday {
+                updatedHabit.currentStreak += 1
+        }else{
+                updatedHabit.currentStreak = 1
+            }
+        
+        // if the current streak is better than best then set best
+        if updatedHabit.currentStreak > updatedHabit.bestStreak {
+            updatedHabit.bestStreak = updatedHabit.currentStreak
+        }
+    
+        // sets last completion to today
+        let now = Date()
+        updatedHabit.lastCompletionDate = now
+        
+        // place the habit back into the array
+        self.habits[habitIndex] = updatedHabit
+        
+        self.saveHabits()
+        
+        return updatedHabit
+    }
+    
+    mutating func swapHabits(habitIndex: Int, destinationIndex: Int){
+        let habitToSwap = self.habits[habitIndex]
+        self.habits.remove(at: habitIndex)
+        self.habits.insert(habitToSwap, at: destinationIndex)
+        self.saveHabits()
+        
+    }
+    
+    mutating func setNeedsToReloadHabits(){
+        self.loadHabits()
+    }
+    
 }
