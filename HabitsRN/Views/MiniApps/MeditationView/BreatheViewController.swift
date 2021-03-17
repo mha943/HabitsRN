a2484639b431c9645d8d1741c926993196827a62
Matthew Hall <matthewhall2943@gmail.com>
2021-03-16 20:09:10 -0600

    Pretty much finished breathe animation. added slider that controls how fast the breathing animation goes

--- HabitsRN/Views/MiniApps/MeditationView/BreatheViewController.swift
+++ HabitsRN/Views/MiniApps/MeditationView/BreatheViewController.swift
@@ -9,54 +9,84 @@ import SwiftUI
 
 class BreatheViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var slider: UISlider!
     
-    let contentView = UIHostingController(rootView: myContentView())
+    static var speed: Double!
+    var contentView = UIHostingController(rootView: myContentView())
     override func viewDidLoad() {
         super.viewDidLoad()
-        contentView.view.center = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height/2)
-        
-        addChild(contentView)
-        view.addSubview(contentView.view)
+        slider.value = 3.25
+        startAnimating()
         view.backgroundColor = UIColor.black
+        BreatheViewController.speed = Double(slider.value)
         
     }
+    
+    func startAnimating(){
+        contentView = UIHostingController(rootView: myContentView())
+        let screenWidth  = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        contentView.view.center = CGPoint(x: screenWidth/2, y: screenHeight/2)
+        addChild(contentView)
+        view.addSubview(contentView.view)
+    }
+    //https://www.swiftbysundell.com/basics/child-view-controllers/
+    // turn off animation when navigating away.
+    func stopAnimating(content: UIViewController) {
+        content.willMove(toParent: nil)
+        content.view.removeFromSuperview()
+        content.removeFromParent()
+    }
+    
+    @IBAction func sliderRelease(_ sender: Any) {
+        stopAnimating(content: contentView)
+        BreatheViewController.speed = Double(slider.value)
+        startAnimating()
+    }
+    
 }
 
+// SwiftUI code for Breathe Animation
 struct myContentView : View {
     @State var isCenter = true
     @State var isCenter2 = true
     
     var body: some View{
         ZStack{
+            //sets size of circle and how much it shrinks/grows
             let size: CGFloat = 150
-            let shrink: CGFloat = 0.4
+            let shrink: CGFloat = 0.25
             let swell: CGFloat = 1.1
-            
-                //right
-            Circle()
+            //let lightGreen = UIColor(displayP3Red: 0.1, green: 1, blue: 0.1, alpha: 0.2)
+
+            Circle()//right
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
                 .offset(x: isCenter ? 0 : 75)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//down
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
                 .offset(y: isCenter ? 0 : 75)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//left
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
                 .offset(x: isCenter ? 0 : -75)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//up
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
                 .offset(y: isCenter ? 0 : -75)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//upleft
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
@@ -64,6 +94,7 @@ struct myContentView : View {
                 .offset(x: isCenter ? 0 : -50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//upright
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
@@ -71,6 +102,7 @@ struct myContentView : View {
                 .offset(x: isCenter ? 0 : 50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//downright
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
@@ -78,6 +110,7 @@ struct myContentView : View {
                 .offset(x: isCenter ? 0 : 50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
             Circle()//downleft
                 .frame(width: size, height: size)
                 .foregroundColor(Color.green.opacity(0.3))
@@ -85,15 +118,15 @@ struct myContentView : View {
                 .offset(x: isCenter ? 0 : -50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
                 .scaleEffect(isCenter ? shrink : swell)
+                .blur(radius: 1.0)
+                
         }
         .onAppear(){
-            let animation = Animation.linear(duration: 4).delay(0.75)
+            let animation = Animation.linear(duration: BreatheViewController.speed).delay(0.5)
             withAnimation(animation.repeatForever(autoreverses: true)){
                 self.isCenter = false
                 self.isCenter2 = false
             }
-            
-            
         }
     }
 }
