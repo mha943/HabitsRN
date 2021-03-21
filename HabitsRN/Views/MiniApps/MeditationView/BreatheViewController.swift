d5e43c9e978704178d724ca791cafc5550812ac1
Matthew Hall <matthewhall2943@gmail.com>
2021-03-20 18:33:51 -0600

    Fiddled with breathing animation again. Better aligned the Circles so that they stack overlapped in the middle during full expansion.

--- HabitsRN/Views/MiniApps/MeditationView/BreatheViewController.swift
+++ HabitsRN/Views/MiniApps/MeditationView/BreatheViewController.swift
@@ -50,84 +50,92 @@ class BreatheViewController: UIViewController {
 struct myContentView : View {
     @State var isCenter = true
     @State var isCenter2 = true
+    //    @State var randomColor: Color = Color(red: Double.random(in: 100...255)/255, green: Double.random(in: 150...255)/255, blue: Double.random(in: 100...255)/255, opacity: 0.2)
+    @State var circleColor: Color = Color(red: 0/255, green: 255/255, blue: 100/255, opacity: 0.25)
     
     var body: some View{
         ZStack{
             //sets size of circle and how much it shrinks/grows
             let size: CGFloat = 150
-            let shrink: CGFloat = 0.25
+            let shrink: CGFloat = 0.3
             let swell: CGFloat = 1.1
             
             Circle()//Background Circle
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.15))
-                //.offset(x: isCenter ? 0 : 75)
-                //.rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
+                .foregroundColor(self.circleColor)
                 .scaleEffect(isCenter ? shrink/4 : swell*2.2)
                 .blur(radius: 5)
+            
             Circle()//right
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(x: isCenter ? 0 : 75)
+                .foregroundColor(self.circleColor)
+                .offset(x: isCenter ? 0 : 70)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//down
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : 75)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : 70)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//left
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(x: isCenter ? 0 : -75)
+                .foregroundColor(self.circleColor)
+                .offset(x: isCenter ? 0 : -70)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//up
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : -75)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : -70)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//upleft
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : -60)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : -50)
                 .offset(x: isCenter ? 0 : -50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//upright
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : -60)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : -50)
                 .offset(x: isCenter ? 0 : 50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//downright
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : 60)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : 50)
                 .offset(x: isCenter ? 0 : 50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
+            
             Circle()//downleft
                 .frame(width: size, height: size)
-                .foregroundColor(Color.green.opacity(0.2))
-                .offset(y: isCenter ? 0 : 60)
+                .foregroundColor(self.circleColor)
+                .offset(y: isCenter ? 0 : 50)
                 .offset(x: isCenter ? 0 : -50)
                 .rotationEffect(Angle(degrees: isCenter2 ? 0 : -270))
                 .scaleEffect(isCenter ? shrink : swell)
                 .blur(radius: 1)
-
         }
         .onAppear(){
+//            let animation = Animation.easeInOut(duration: BreatheViewController.speed).delay(0.5)
             let animation = Animation.linear(duration: BreatheViewController.speed).delay(0.5)
             withAnimation(animation.repeatForever(autoreverses: true)){
                 self.isCenter = false
